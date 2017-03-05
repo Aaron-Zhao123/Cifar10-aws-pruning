b08718b9ce8ce434c33f442d3b85aa4e80cd8e66
Aaron Zhao <aaron@dhcp-10-249-115-22.eduroam.wireless.private.cam.ac.uk>
2017-03-05 18:27:40 +0000

    test version

--- train.py
+++ train.py
@@ -531,7 +531,8 @@ def main(argv = None):
             print(78*'-')
             start = time.time()
             if TRAIN == 1:
-                for i in range(0,60000):
+                # for i in range(0,60000):
+                for i in range(0,60):
                     (batch_x, batch_y) = t_data.feed_next_batch(BATCH_SIZE)
                     train_acc, cross_en = sess.run([accuracy, loss_value], feed_dict = {
                                     x: batch_x,
