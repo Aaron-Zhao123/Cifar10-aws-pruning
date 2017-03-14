f021480a3e7732faa6cfc74b067fa0b42b35f940
Aaron Zhao <aaron@user-32-56.vpn.net.private.cam.ac.uk>
2017-03-14 16:08:03 +0800

    run

--- run.py
+++ run.py
@@ -206,4 +206,4 @@ print('accuracy summary: {}'.format(acc_list))
 # acc_list = [0.82349998, 0.8233, 0.82319999, 0.81870002, 0.82050002, 0.80400002, 0.74940002, 0.66060001, 0.5011]
 with open("acc_cifar.txt", "w") as f:
     for item in acc_list:
-        f.write("%s\n"%item)
+        f.write("%s %s %s\n"%item[0]%item[1]%item[2])
