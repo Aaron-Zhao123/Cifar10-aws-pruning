bdd4a648d872ba6876d94d93d87c3b7cd181b554
Aaron Zhao <aaron@user-32-56.vpn.net.private.cam.ac.uk>
2017-03-25 13:01:36 +0800

    update test

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ def compute_file_name(pcov, pfc):
 acc_list = []
 count = 0
 pcov = [0., 0.]
-pfc = [0., 0., 0.]
+pfc = [60., 0., 0.]
 retrain = 0
 f_name = compute_file_name(pcov, pfc)
 lr = 1e-5
@@ -70,7 +70,7 @@ level6 = 0
 working_level = level1
 hist = [(pcov, pfc, test_acc)]
 pcov = [0., 0.]
-pfc = [0., 0., 0.]
+pfc = [60., 0., 0.]
 retrain_cnt = 0
 roundrobin = 0
 # Prune
