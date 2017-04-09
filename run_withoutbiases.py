49a05cb09229d3e1b70c03c666ed95294ca7329d
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-09 11:35:32 +0100

    added without biases assets

--- run_withoutbiases.py
+++ run_withoutbiases.py
@@ -23,7 +23,7 @@ pcov = [0., 0.]
 pfc = [0., 0., 0.]
 retrain = 0
 f_name = compute_file_name(pcov, pfc)
-parent_dir = 'assets/withbiases/'
+parent_dir = 'assets/withoutbiases/'
 # lr = 1e-5
 lr = 1e-4
 
