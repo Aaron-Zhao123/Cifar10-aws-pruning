1a311973fbbc194e4c883882134af407e399d4d8
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-13 09:31:24 +0100

    added without biases assets

--- run.py
+++ run.py
@@ -74,8 +74,10 @@ level6 = 0
 
 working_level = level1
 hist = [(pcov, pfc, test_acc)]
-pcov = [20., 66.]
-pfc = [85., 66., 20.]
+# pcov = [20., 66.]
+# pfc = [85., 66., 20.]
+pcov = [0., 0.]
+pfc = [70., 0., 0.]
 retrain_cnt = 0
 roundrobin = 0
 with_biases = True
@@ -138,12 +140,12 @@ while (run):
     f_name = compute_file_name(pcov, pfc)
     # pcov[1] = pcov[1] + 10.
     if (acc > 0.823):
-        # pfc[0] = pfc[0] + 10.
+        pfc[0] = pfc[0] + 5.
         # pfc[0] = pfc[0] + 1.
         # pfc[1] = pfc[1] + 10.
         # pcov[1] = pcov[1] + 10.
-        pfc[2] = pfc[2] + 10.
-        pcov[0] = pcov[0] + 10.
+        # pfc[2] = pfc[2] + 10.
+        # pcov[0] = pcov[0] + 10.
         lr = 1e-4
         retrain = 0
         roundrobin = 0
