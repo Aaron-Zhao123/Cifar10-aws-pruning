80d5e68f50f5cd402c90b6e2baec80028122ed2a
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-13 17:23:14 +0100

    added without biases assets

--- run_withoutbiases.py
+++ run_withoutbiases.py
@@ -20,7 +20,7 @@ def compute_file_name(pcov, pfc):
 acc_list = []
 count = 0
 pcov = [0., 0.]
-pfc = [75., 0., 0.]
+pfc = [0., 0., 0.]
 retrain = 0
 f_name = compute_file_name(pcov, pfc)
 parent_dir = 'assets/withoutbiases/'
@@ -76,7 +76,7 @@ level6 = 0
 working_level = level1
 hist = [(pcov, pfc, test_acc)]
 pcov = [0., 0.]
-pfc = [80., 0., 0.]
+pfc = [10., 0., 0.]
 retrain_cnt = 0
 roundrobin = 0
 # Prune
@@ -138,7 +138,7 @@ while (run):
     f_name = compute_file_name(pcov, pfc)
     # pcov[1] = pcov[1] + 10.
     if (acc > 0.823):
-        pfc[0] = pfc[0] + 1.
+        pfc[0] = pfc[0] + 10.
         # pfc[0] = pfc[0] + 1.
         # pfc[1] = pfc[1] + 10.
         # pcov[1] = pcov[1] + 10.
